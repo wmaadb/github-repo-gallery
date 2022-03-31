6f65925e5ef042d882a991b6521049faf4854dde
Wanda <wmaadb@gmail.com>
2022-03-30 19:29:27 -0600

    Add user data

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+//[name,bio,location,number of public repos]
+const overview = dcoument.querySelector(".overview");
+const username = "wmaadb";
+
+const gitUserInfo = async function () {
+  const userInfo = await fetch(`https://api.github.com/users/${username}`);
+  const data = await userInfo.json();
+  displayUserInfo(data);
+};
+
+gitUserInfo();
+
+const displayUserInfo = function (data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+        <figure>
+            <img alt="user avatar" src=${data.avatar_url} />
+        </figure>
+    <div>
+        <p><strong>Name:</strong> ${data.name}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p>
+        <p><strong>Location:</strong> ${data.location}</p>
+        <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div> 
+    `;
+    overview.append(div);
+};
